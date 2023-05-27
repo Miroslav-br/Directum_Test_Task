5b32c92cc6ed679d718685a349debe25a1806b2a
Miroslav Bodnar <mirik01@mail.ru>
2023-05-27 22:56:51 +0400

    update project

--- TextFileCreator/Program.cs
+++ TextFileCreator/Program.cs
@@ -4,41 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Initialize();
-        }
-
-        private static void Initialize()
-        {
             Console.WriteLine("Напишите ниже формат файлов, который вы бы хотели создать (txt | docx)");
-            string? inputvalue = Console.ReadLine()?.Trim().ToLower();
             IFilesFactory factory;
-            switch (inputvalue)
-            {
-                case nameof(FileExtensions.docx):
-                    factory = DocxCreator.GetCreator();
+            while (true)
+            {   string? inputvalue = Console.ReadLine()?.Trim().ToLower();
+                switch (inputvalue)
+                {
+                    case nameof(FileExtensions.docx):
+                        factory = DocxCreator.GetCreator();
+                        break;
+                    case nameof(FileExtensions.txt):
+                        factory = TxtCreator.GetCreator();
+                        break;
+                    default:
+                        Console.WriteLine("С таким форматом данных на данный момент программа не работает\nПоробуйте снова");
+                        continue;
+                }
+                if(factory != null)
+                {
+                    factory.CreateFiles();
+                    Console.WriteLine("Файлы успешно созданы");
                     break;
-                case nameof(FileExtensions.txt):
-                    factory = TxtCreator.GetCreator();
-                    break;
-                default:
-                    Console.WriteLine("С таким форматом данных на данный момент программа не работает\nХотите выбрать другой формат? y/n");
-                    RepeatChooseOrNot();
-                    return;
+                }
             }
-            factory.CreateFiles();
-            Console.WriteLine("Файлы успешно созданы");
-        }
-
-        private static void RepeatChooseOrNot()
-        {
-            char answer = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            if (answer == 'y')
-            {
-                Initialize();
-            }
-
-            return;
         }
     }
 }
